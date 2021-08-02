c094c21b85145272bda0e3d52b65b87171b29aed
sipachevaop <sipachevaop@intech.rshb.ru>
2021-08-03 02:57:01 +0300

    Homework 4 DragAndDrop added

---
+++ src/test/java/guru/qa/DragAndDrop.java
@@ -0,0 +1,19 @@
+package guru.qa;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDrop {
+
+    @Test
+    void draganddrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+
+    }
+}
